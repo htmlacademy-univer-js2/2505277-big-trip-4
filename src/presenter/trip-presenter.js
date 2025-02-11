efe9c6c850be5a80caf65319742f474e83aed08e
4us4e <145545210+4us4er@users.noreply.github.com>
2025-02-11 23:49:22 +0700

    fix: render li to ul n pulling up changes

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -5,7 +5,7 @@ import SortingView from '../view/sorting.js';
 import FilterView from '../view/filters.js';
 import EditingFormView from '../view/editingForm.js';
 import WaypointView from '../view/waypoint.js';
-
+import ListView from '../view/creationList.js';
 import TripInfoView from '../view/tripInfoView.js';
 
 const header = document.querySelector('.page-header');
@@ -19,16 +19,19 @@ render(new TripInfoView(), tripMain, RenderPosition.AFTERBEGIN);
 render(new FilterView(), tripMain, RenderPosition.BEFOREEND);
 
 export default class TripPlannerPresenter {
+  listComponent = new ListView();
   constructor({ TripPlannerContainer }) {
     this.TripPlannerContainer = TripPlannerContainer;
   }
 
   init() {
-    render(new SortingView(), this.TripPlannerContainer);
-    render(new EditingFormView(), this.TripPlannerContainer);
-    render(new CreationFormView(), this.TripPlannerContainer);
+    render(this.listComponent, this.TripPlannerContainer);
+
+    render(new SortingView(), this.listComponent.getElement());
+    render(new EditingFormView(), this.listComponent.getElement());
+    render(new CreationFormView(), this.listComponent.getElement());
     for (let i = 0; i < 3; i++) {
-      render(new WaypointView(), this.TripPlannerContainer);
+      render(new WaypointView(), this.listComponent.getElement());
     }
   }
 }
