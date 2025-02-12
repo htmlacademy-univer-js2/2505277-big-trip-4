b7d7623c59a7f2ce97494eb50edcc2f5db64e461
4us4e <145545210+4us4er@users.noreply.github.com>
2025-02-12 20:37:00 +0700

    deleted unnecessary files

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -5,7 +5,7 @@ import SortingView from '../view/sorting.js';
 import FilterView from '../view/filters.js';
 import EditingFormView from '../view/editingForm.js';
 import WaypointView from '../view/waypoint.js';
-import ListView from '../view/creationList.js';
+import WaypoinListView from '../view/creationList.js';
 import TripInfoView from '../view/tripInfoView.js';
 
 const header = document.querySelector('.page-header');
@@ -15,19 +15,18 @@ const siteContainerElement = siteMainElement.querySelector(
   '.page-body__container'
 );
 
-render(new TripInfoView(), tripMain, RenderPosition.AFTERBEGIN);
-render(new FilterView(), tripMain, RenderPosition.BEFOREEND);
-
 export default class TripPlannerPresenter {
-  listComponent = new ListView();
+  listComponent = new WaypoinListView();
   constructor({ TripPlannerContainer }) {
     this.TripPlannerContainer = TripPlannerContainer;
   }
 
   init() {
+    render(new TripInfoView(), tripMain, RenderPosition.AFTERBEGIN);
+    render(new FilterView(), tripMain, RenderPosition.BEFOREEND);
+    render(new SortingView(), this.TripPlannerContainer);
     render(this.listComponent, this.TripPlannerContainer);
 
-    render(new SortingView(), this.listComponent.getElement());
     render(new EditingFormView(), this.listComponent.getElement());
     render(new CreationFormView(), this.listComponent.getElement());
     for (let i = 0; i < 3; i++) {
